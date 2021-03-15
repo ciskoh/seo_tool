6a3c5f35009948911a125d6b7526ebade391b09b
ciskoh <matteo.jriva@gmail.com>
2021-03-15 17:12:51 +0100

    modified licence and added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#   /********************************************************************************
+#   * Copyright © 2021-2022, Matteo Jucker Riva
+#   * All rights reserved. This program and the accompanying materials
+
+#   * Contributors:
+#   *     Matteo Jucker Riva, Ph.D.
+#   *******************************************************************************/
+
+# Main script to run the no_answer_tool
+def main():
+    pass
+if __name__ == '__main__':
+    main()
