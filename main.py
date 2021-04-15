58297e4ae88ea0d1669bd27db0d1b0c80246b555
ciskoh <matteo.jriva@gmail.com>
2021-04-15 10:16:24 +0200

    updated main.py to include ppa question retrieval

--- main.py
+++ main.py
@@ -5,15 +5,29 @@
 #   * Contributors:
 #   *     Matteo Jucker Riva, Ph.D.
 #   *******************************************************************************/
-from src.data import get_question
-from src.data import generate_automatic_questions
+from src.data import get_question, generate_automatic_questions, get_ppas_and_answers
 from pathlib import Path
 # Main script to run the no_answer_tool
 def main():
+    # step 1 get input from user
     q = get_question()
+
+    # step 2 generate automatic questions
     gen_qs = generate_automatic_questions(q.keywords, Path("references", "words_for_questions.csv"))
     q.related_questions += gen_qs
     print(q.related_questions)
-    pass
+    # step 3 get people also ask questions
+    ppas = get_ppas_and_answers(q.related_questions, mode="ppa")['ppa']
+    print(ppas)
+    ppas_flattened =set([q for sublist in ppas for q in sublist])
+    q.related_questions +=ppas_flattened
+    answers = get_ppas_and_answers(q.related_questions, mode="link")['link']
+    for x,a in zip(q.related_questions, answers):
+        print("\n")
+        print(x, ":")
+        print("\n".join(a[:20]))
+
+
+
 if __name__ == '__main__':
     main()
