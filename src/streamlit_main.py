95eab81bea280b5570ef9e450c9ec90515ffbea1
ciskoh <matteo.jriva@gmail.com>
2021-04-27 11:47:36 +0200

    cerate streamlit_main.py and adjusted a lot of stuff to make it work

--- src/streamlit_main.py
+++ src/streamlit_main.py
@@ -14,11 +14,13 @@ This tool explores Google to check which have no defined answer
 Input your question or keyword below and let the tool do the rest
 """
 
+
 @st.cache(allow_output_mutation=True)
 def create_flow_control(n):
     """returns a list for flow control. The list can be mutated outside
     the function and will remain in memory"""
-    return [False]*n
+    return [False] * n
+
 
 @st.cache(allow_output_mutation=True)
 def get_ppa_streamlit(user_input):
@@ -31,13 +33,23 @@ def get_ppa_streamlit(user_input):
     query = main_get_questions(user_input)
     return query
 
+#TODO: remove unwanted questons does not behave properly
 @st.cache(allow_output_mutation=True)
-def streamlit_get_answers(query):
+def streamlit_get_answers(query, keep_qs):
+    """get answers only for selected questions
+    Parameters:
+    query: Question Holder object
+    keep_qs: Questions to keep"""
+    print("before removing", len(query.questions))
+    query.remove_unwanted_questions(keep_qs, "k")
+    print("after removing", len(query.questions))
+    print(str(query))
     return main_get_answers(query)
+
 
 def streamlit_main():
     """main function to create a streamlit GUI"""
-    flow_control=create_flow_control(4)
+    flow_control = create_flow_control(4)
 
     if st.button("Press to Start") and not any(flow_control):
         flow_control[0] = True
@@ -52,16 +64,17 @@ def streamlit_main():
         * Keywords can be any number of words, divided by a space    
         """)
         user_input = st.text_input("Enter a question or keywords", "")
-        if st.button("Confirm") or flow_control[1]:
-            st.warning("Downloading most relevant questions from Google.\n This can take up to 1 minute")
-            query = get_ppa_streamlit(user_input)
-            question_list=query.to_list("q")
-            if question_list:
-                st.success("All question retrieved")
-                st.write(question_list)
-                flow_control[1] = True
-                flow_control[2] = True
+        if st.button("Confirm"):
+            flow_control[1] = True
 
+    if flow_control[1]:
+        st.warning("Downloading most relevant questions from Google.\n This can take up to 1 minute")
+        query = get_ppa_streamlit(user_input)
+        question_list = query.to_list("q")
+        if question_list:
+            st.success("All question retrieved")
+            st.write(question_list)
+            flow_control[2] = True
 
     if flow_control[2]:
         st.markdown("""## Step 2. Filter your questions. 
@@ -75,16 +88,17 @@ def streamlit_main():
         keep_qs = st.multiselect("Choose the relevant questions", question_list)
         st.write('You selected:', keep_qs)
         # breakpoint()
-        if st.button("confirm selected questions") or flow_control[3]:
-            st.warning("Downloading most relevant answers from Google.\n This can take up to 1 minute")
-            query.remove_unwanted_questions(keep_qs, "k")
-            print("streamlit "+str(type(query)))
-            query = streamlit_get_answers(query)
-            if query:
-                st.success("All answers downloaded")
-                flow_control[3]=True
-                st.write(str(query))
+        if st.button("confirm selected questions"):
+            flow_control[3]=True
 
+    if flow_control[3]:
+        st.warning("Downloading most relevant answers from Google.\n This can take up to 1 minute")
+
+        query = streamlit_get_answers(query, keep_qs)
+        if query:
+            st.success("Your data is ready")
+            st.write(str(query))
+            st.write(query.to_pandas())
 
 
 if __name__ == '__main__':
